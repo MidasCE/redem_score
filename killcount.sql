ba872d6322929fe29ee4fbb1ae1403326520e112
MidasCE <midaskmitl@gmail.com>
2019-12-15 22:09:26 +0700

    Fix bug in killcount.sql

--- killcount.sql
+++ killcount.sql
@@ -5,7 +5,7 @@ USE `redemrp`;
 CREATE TABLE IF NOT EXISTS `character_score` (
   `identifier` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
   `characterid` int(11) DEFAULT 0,
-  `kill` int(11) DEFAULT 0,,
+  `kill` int(11) DEFAULT 0,
   `death` int(11) DEFAULT 0,
   `score` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
